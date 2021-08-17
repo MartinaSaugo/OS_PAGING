3a3797203b779e0ccc47942b97bd7133415cbdde
MartinaSaugo <s285326@studenti.polito.it>
2021-08-17 12:29:16 +0200

    trash

--- kern/include/coremap.h
+++ kern/include/coremap.h
@@ -8,7 +8,7 @@ typedef struct coremap_entry {
 	status_t status; 
 	paddr_t paddr; //where?
 	long size;  //= previous allocsize, now all in one
-  int next;   // where is the next frame allocated?
+  	int next;   // where is the next frame allocated?
 	//potential other info for paging
 } coremap_entry_t;
 
