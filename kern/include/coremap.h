361002bbf4eb03d6d968c868fd359727f823db4d
laib3 <54323877+laib3@users.noreply.github.com>
2021-08-23 09:42:45 +0200

    feat: start implementing pagetable

--- kern/include/coremap.h
+++ kern/include/coremap.h
@@ -2,21 +2,13 @@
 #define _COREMAP_H_
 #include <machine/vm.h> //why?
 
-
 typedef enum {FREE, CLEAN, DIRTY, FIXED} status_t;
 
 typedef struct coremap_entry {
 	status_t status; 
-	paddr_t paddr; //where?
-	long size; //= previous allocsize, now all in one
-	//potential other info for paging
-	int next; //next part of contiguous memory
-}coremap_entry_t;
+	paddr_t paddr;  //where?
+	long size;      //= previous allocsize, now all in one
+} coremap_entry_t;
 
-/*declare
-//extern coremap_entry_t *freeRamFrames;
-coremap_entry_t *freeRamFrames;
-long nRamFrames;
-int allocTableActive = 0;*/
 
 #endif
---
+++ kern/include/pagetable.h
@@ -0,0 +1,16 @@
+#ifndef _PAGETABLE_H_
+#define _PAGETABLE_H_
+
+typedef struct pagetable_entry {
+  vaddr_t vaddr;                  // starting virtual address of the page 
+  int ppage_index;                // maps a virtual page to a physical page (-> coremap)
+  struct pagetable_entry *next;   // implement as linked list
+} pagetable_entry_t; 
+
+typedef struct pagetable {
+  struct pagetable_entry *head;
+  struct pagetable_entry *tail;
+  int npages;
+} pagetable_t;
+
+#endif
