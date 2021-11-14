5d1a69d2809dae97be618187aeaf6183785b3d6d
laib3 <54323877+laib3@users.noreply.github.com>
2021-11-14 12:47:31 +0100

    refactoring: add some comments, implement pagetable based on list with sentinel

--- kern/vm/pt.c
+++ kern/vm/pt.c
@@ -15,44 +15,55 @@ pt_t* pt_init(void)
  * @return -1 if not found, -2 if swapped,  
  * a positive integer if found (the index of the ppage in the coremap). */
 int pt_search(pt_t *pt, vaddr_t vaddr) {
-    ptentry_t *pe;
-    if(pt -> head == NULL)  // list is empty
-        return -1;
-    for(pe = pt -> head; pe != NULL; pe = pe -> next){
-    if(vaddr >= pe -> vaddr && vaddr < pe -> vaddr + PAGE_SIZE){
-        if(pe -> status == SWAPPED)
-            return -2;
-        return pe -> ppage_index;
+    ptentry_t *pte;
+    for(pte = pt -> nil -> next; pte != pt -> nil; pte = pte -> next){
+        /* check if the current entry corresponds to vaddr */
+        if(vaddr >= (pte -> vaddr) && vaddr < (pte -> vaddr) + PAGE_SIZE){
+            if(pte -> status == SWAPPED)
+                return -2;
+            return pte -> ppage_index;
+        }
     }
-  }
-  return -1;
+    /* list is empty or the entry is not found */
+    return -1;
 }
 
-/* add an entry to the pagetable, also allocates page in memory (dynamic allocation) - called only when you're sure there's enough space */
+/* add an entry to the pagetable, 
+ * also allocate page in memory (dynamic allocation) - 
+ * this function is called only when you're sure there's 
+ * enough space 
+ * */
 int pt_add(pt_t *pt, vaddr_t vaddr) {
     paddr_t paddr;
-    ptentry_t *p;     
+    ptentry_t *new;
     int index = -1;
-    paddr = getppages(1);   // get a new page, notice that you don't know where it will be in physical memory
+    // get a new page, notice that you don't know where it will be in physical memory
+    paddr = getppages(1);   
     // TODO checks
     index = paddr / PAGE_SIZE;
-    // create a new pte and update its fields 
-    p = kmalloc(sizeof(ptentry_t));
-    p -> vaddr = vaddr & PAGE_FRAME;  // get starting vaddr of vpage
-    p -> ppage_index = index;
-    p -> next = pt -> head;
-    p -> status = PRESENT;
-    pt -> head = p;
+    /* create the new entry */
+    new = (ptentry_t *) kmalloc(sizeof(ptentry_t));
+    new -> vaddr = vaddr & PAGE_FRAME;
+    new -> status = PRESENT;
+    /* attach the node to the rest of the list */
+    new -> next = pt -> nil -> next;
+    /* put new entry as first element of the list */
+    pt -> nil -> next = new;
     return index;
 }
 
-/* implement a round-robin-like victim selection algorithm 
- * (if at the end, start from head) */
+/* implement a round-robin-like victim selection algorithm */
 ptentry_t *pt_select_victim(pt_t *pt){
+    /* pagetable is empty */
+    if(pt -> nil -> next == pt -> nil)
+        return NULL;
     static ptentry_t *next_victim = NULL;
-    if(next_victim == NULL){
-        next_victim = pt -> head;
+    next_victim = pt -> nil;
+    /* restart from the top */
+    if(next_victim == pt -> nil){ 
+        next_victim = next_victim -> next;
     }
+    /* copy the victim */
     ptentry_t *victim = next_victim; 
     next_victim = next_victim -> next;
     return victim;
